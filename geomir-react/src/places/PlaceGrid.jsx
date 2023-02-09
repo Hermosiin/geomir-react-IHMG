5cc573ea5b55cc03b5e1a932f25e484f65b24d04
Ismael Hermoso Rivas <ismael.hermoso.rivas@hotmail.com>
2023-02-09 16:40:36 +0100

    cambio place grid

--- geomir-react/src/places/PlaceGrid.jsx
+++ geomir-react/src/places/PlaceGrid.jsx
@@ -9,15 +9,14 @@ export const PlaceGrid = ({place, deletePlace}) => {
   return (
     <>
         <div className="grid-place">
-          <p>{place.author.name}</p>
-          <h2>{place.name}</h2>
-            <img src={"https://backend.insjoaquimmir.cat/storage/" + place.file.filepath} alt={place.name} height="400"width="300"/>
-            <div>
-                {place.description}     
-            </div>
-            <div>
-                {place.favorites_count} <i className="bi bi-star-fill"></i>
-            </div>
+            <img src={"https://backend.insjoaquimmir.cat/storage/" + place.file.filepath} alt={place.name} height="450"width="450"/>
+            <h1>{place.name}</h1>
+            <p>Author: {place.author.name}</p>
+            <p>Description: {place.description}</p>
+            <p>Latitude: {place.latitude}</p>
+            <p>Longitude: {place.longitude}</p>
+            <p>Favorites: {place.favorites_count} <i className="bi bi-star-fill"></i></p>
+            <p>Este place tiene {place.reviews_count} reseñas.</p>
             <div>
                 <Link to={"/places/" +place.id}> <i className="bi bi-eye-fill"></i></Link>
 
